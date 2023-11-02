3a92de7a050822b2c9c7342745a31df8e08f087c
Spencer Rauch <rauchs@gmail.com>
2023-11-02 10:34:08 -0700

    baking mats stock feature

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -55,7 +55,41 @@ public class InventoryController : Controller
         BakingMaterial? BMfromDb = _context.BakingMaterials
                                             .Include(bm => bm.Stock)
                                             .FirstOrDefault(bm => bm.BakingMaterialId == id);
-        return View("BakingMaterialView",BMfromDb);
+        return View("BakingMaterialView", BMfromDb);
+    }
+
+    //BAKING MATERIAL STOCK
+    [HttpGet("bakingmaterialstock/new/{bmId}")]
+    public ViewResult BakingMaterialStockNew(int bmId)
+    {
+        BakingMaterial bm = _context.BakingMaterials.FirstOrDefault(bm => bm.BakingMaterialId == bmId);
+        return View("BakingMaterialStockNew", bm);
+    }
+
+    [HttpPost("bakingmaterialstock/create/{bmId}")]
+    public IActionResult BakingMaterialStockCreate(BakingMaterialStockForm bmsForm, int bmId)
+    {
+        if (!ModelState.IsValid)
+        {
+            var message = string.Join(" | ", ModelState.Values
+                                .SelectMany(v => v.Errors)
+                                .Select(e => e.ErrorMessage));
+            Console.WriteLine(message);
+            return BakingMaterialStockNew(bmId);
+        }
+        BakingMaterialStock newStock = new()
+        {
+            BakingMaterialId = bmId,
+            Quantity = bmsForm.Quantity,
+            Brand = bmsForm.Brand,
+            Batch = bmsForm.Batch,
+            DateOfManufacture = bmsForm.DateOfManufacture,
+            Expiration = bmsForm.Expiration,
+            Status = 0
+        };
+        _context.Add(newStock);
+        _context.SaveChanges();
+        return RedirectToAction("BakingMaterialView", new { id = bmId });
     }
 
     [HttpPost("bakingmaterialstock/adjust/{returnId}")]
@@ -69,7 +103,7 @@ public class InventoryController : Controller
                                             .FirstOrDefault(bms => bms.BakingMaterialStockId == formAdjust.BakingMaterialStockId);
         if (BMSToAdjust == null)
         {
-            ModelState.AddModelError("Quantity","Error locating batch");
+            ModelState.AddModelError("Quantity", "Error locating batch");
             return BakingMaterialView(returnId);
         }
         BMSToAdjust.Quantity += (int)formAdjust.Quantity;
@@ -81,7 +115,7 @@ public class InventoryController : Controller
         };
         _context.Add(newAdjust);
         _context.SaveChanges();
-        return RedirectToAction("BakingMaterialView",new{id=returnId});
+        return RedirectToAction("BakingMaterialView", new { id = returnId });
     }
 
     //SHIPPING MATERIALS
@@ -92,7 +126,7 @@ public class InventoryController : Controller
     }
 
     [HttpPost("shippingmaterial/create")]
-    public IActionResult ShippingMaterialCreate (ShippingMaterial newSM)
+    public IActionResult ShippingMaterialCreate(ShippingMaterial newSM)
     {
         if (!ModelState.IsValid)
         {
---
+++ Models/ViewModels/BakingMaterialStockForm.cs
@@ -0,0 +1,44 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+
+namespace OatBoySystem.Models.ViewModels;
+public class BakingMaterialStockForm
+{
+    public BakingMaterial? BakingMaterial { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+
+    [Required]
+    public string Brand { get; set; }
+
+    [Required(ErrorMessage = "Batch required. Enter DOM or EXP as MM/YY if batch unknown.")]
+    [UniqueBatch]
+    public string Batch { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? DateOfManufacture { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? Expiration { get;set; }
+
+}
+
+public class UniqueBatchAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return new ValidationResult("Batch required. Enter DOM or EXP as MM/YY if batch unknown.");
+        }
+        OatBoyContext _context = (OatBoyContext)validationContext.GetService(typeof(OatBoyContext));
+        if (_context.BakingMaterialStock.Any(bms => bms.Batch == value.ToString()))
+        {
+            return new ValidationResult("Batch in system, make adjustment instead");
+        }
+        else
+        {
+            return ValidationResult.Success;
+        }
+    }
+}
